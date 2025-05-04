d63f3639ace2fb8e49e147f0d54bc0b17907e003
malumzz <gmalumzz@gmail.com>
2025-05-04 17:40:19 +0200

    Basic structure with sample code

---
+++ src/pages/RegistrationPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegistrationPage = () => {
+  return (
+    <div>
+      <h1>Registration Page</h1>
+      <p>Create a new account</p>
+    </div>
+  );
+};
+
+export default RegistrationPage;
