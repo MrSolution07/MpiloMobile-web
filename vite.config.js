de82cb02bf273d31cb8eba85679fb64425deb180
Thendo A Munyai <60216128+tamunyai@users.noreply.github.com>
2025-05-27 15:06:34 +0200

    fix: website not displaying

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
 });
